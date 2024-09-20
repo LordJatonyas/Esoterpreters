b84cadf2ee24b00d068a74242ab99b0a8c6f5046
Lee Chih Jung <john.12012000@gmail.com>
2024-09-20 15:47:00 +0100

    Building [space] IMP

--- whitespace/stack_manipulation.zig
+++ whitespace/stack_manipulation.zig
@@ -1,1 +1,9 @@
 const std = @import("std");
+
+pub const Stack_IMP = struct {
+    stack: std.ArrayList(i64),
+
+    pub fn push(self: *Stack_IMP, num: i64) void {
+        try self.stack.append(num);
+    }
+};
--- whitespace/whitespace.zig
+++ whitespace/whitespace.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
-const Stack_IMP = @import("./stack_manipulation.zig");
+const stack_manipulation = @import("stack_manipulation.zig");
+
+const allocator = std.debug.global_allocator;
+const warn = std.debug.warn;
 
 pub fn main() void {
     std.debug.print("{s}\n", .{"Hello world!"});
+
+    const Vec64 = std.ArrayList(i64);
+    var stack = Vec64.init(allocator);
+
+    stack_manipulation.Stack_IMP(stack).push(32);
 }
