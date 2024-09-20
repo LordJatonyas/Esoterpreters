484a2f1aeee36fddfa79bd7597ef1d43789aa7b3
Lee Chih Jung <54738966+LordJatonyas@users.noreply.github.com>
2024-09-20 17:17:39 +0100

    inspired by ThePrimeagen, create stack ds

--- whitespace/whitespace.zig
+++ whitespace/whitespace.zig
@@ -1,14 +1,10 @@
 const std = @import("std");
+const print = std.debug.print;
 const stack_manipulation = @import("stack_manipulation.zig");
-
-const allocator = std.debug.global_allocator;
-const warn = std.debug.warn;
 
 pub fn main() void {
     std.debug.print("{s}\n", .{"Hello world!"});
-
-    const Vec64 = std.ArrayList(i64);
-    var stack = Vec64.init(allocator);
-
-    stack_manipulation.Stack_IMP(stack).push(32);
+    const stack = [_]i64{ 1, 2, 3, 4, 5 };
+    const elem = [1]i64{32};
+    print("{any}\n", .{stack ++ elem});
 }
